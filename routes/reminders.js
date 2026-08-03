ea6a788ae6f22ae8bddb7214e71b84ed64212c19
divya <pulamidivya26@gmail.com>
2026-08-03 10:33:07 -0400

    commit added patch route

--- routes/reminders.js
+++ routes/reminders.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Reminder = require("../models/Reminder")
-const Pet = require("../models/Pet")
+const Pet = require("../models/Pet");
+const { UPDATE } = require("sequelize/lib/query-types");
 
 router.get("/", async (req, res) => {
     console.log(Reminder)
@@ -42,5 +43,29 @@ router.delete("/:id", async (req, res) => {
     await reminder.destroy();
     res.sendStatus(204); 
 });
+
+
+router.patch("/:id", async (req, res) => {
+//   const { id } = req.params;
+//   const { task, dueDate, notes } = req.body;
+  try {
+    const reminder = await Reminder.findByPk(req.params.id)
+    if (!reminder) {
+        return res.status(404).json({ error: 'Reminder not found' });
+    }
+   const updatedReminder =  await reminder.update(req.body)
+    // Update database record logic here
+    res.status(200).json({ message: 'Updated successfully'});
+  } catch (err) {
+    res.status(500).json({ error: 'Update failed' });
+  }
+});
+// router.patch("/:id/edit", async (req, res) => {
+//     const id = req.params.id
+//     const reminder = await Reminder.findByPk(id);
+//     if (!reminder) return res.status(404).json({error : "Reminder Not Found"});
+//     await reminder.patch();
+//     res.sendStatus(204); 
+// });
 
 module.exports = router
